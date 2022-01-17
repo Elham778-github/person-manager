8998b80a90b8d1631096199b076f9cdc2152b0c9
elham <elhamrouzban778@yahoo.com>
2022-01-17 10:43:08 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Persons from './components/person/Persons';
 
-export default App;
+class App extends Component {
+    state = { 
+        persons :[
+            {id : 1, fullname : "lida rouzban", age : 40},
+            {id : 2, fullname : "leila shahkarami",  age : 43},
+            {id : 3, fullname : "reza taji",  age : 41}
+        ], showDiv : false
+    }
+    handel = () => {
+        this.setState({showDiv : !this.state.showDiv});
+        
+    }   
+    deletPerson = id =>{
+        const persons = [...this.state.persons];
+        const filterPerson = persons.filter(person => (person.id !== id));
+        this.setState({persons : filterPerson});
+    } 
+    handelNameChange = (event, id)=>{
+        const Person
+
+    }
+
+    
+    render(){
+        const {persons, showDiv} = this.state;
+        let person =null;
+        if(showDiv){
+            person = <Persons persons={persons}  deletPerson={this.deletPerson} />
+
+
+        }
+        const buttonstyle = {
+            padding :"8px",
+            backgroundColor : "pink"
+        }
+
+        
+        return(
+            <div>
+                <h2>persons managment</h2>
+                <h3>there is {persons.length} persons</h3>
+                {person}
+            <button onClick={this.handel} style={buttonstyle}> click on me</button>
+            <button onClick={this.deletPerson} style={buttonstyle}> delet</button>
+            
+            </div>
+            );
+        };
+        
+        
+        
+    }
+    
+    export default App;
+    
+    
