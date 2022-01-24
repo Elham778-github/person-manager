36e99bf16db7bc8d100c4e3138a95db5470218ea
elham <elhamrouzban778@yahoo.com>
2022-01-24 22:49:57 +0100

    added provider & consumer

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { Badge, Alert } from "react-bootstrap";
+import SimpleContext from "../../contex/simpleContex";
+import Persons from '../person/Persons';
 
 
-const Header  = ({personsLengt, appTitle}) => {
-    let badgeStyle = "";
-    if(personsLengt >=3)badgeStyle="bg-success";
-    if(personsLengt <=2)badgeStyle="bg-warning";
-    if(personsLengt <=1)badgeStyle="bg-danger";
-    
-  
-    return (
+const Header = ({ personsLengt}) => {
+  let badgeStyle = "";
+  if (personsLengt >= 3) badgeStyle = "bg-success";
+  if (personsLengt <= 2) badgeStyle = "bg-warning";
+  if (personsLengt <= 1) badgeStyle = "bg-danger";
+
+
+  return (
+    <SimpleContext.Consumer  >
+      {context =>(
+              <div>
+              <Alert variant="info">
+                <h2>{context.state.appTitle}</h2>
+              </Alert>
+      
+              <Alert variant="light" >
+                there is
+                <Badge pill className={badgeStyle}>
+                  {context.state.persons.length}
+                </Badge>persons
+              </Alert>
+      
+            </div>
+      
         
-        <div>
-        <Alert variant="info">
-          <h2>{appTitle}</h2>
-        </Alert>
+      )}
+    </SimpleContext.Consumer>
 
-        <Alert variant="light" >
-        there is
-          <Badge pill className={badgeStyle}>
-            {personsLengt}
-          </Badge>persons
-        </Alert>
-
-        </div>
-
-      );
+  );
 }
- 
-export default Header ;
+
+export default Header;
