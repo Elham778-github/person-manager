84447d9a90199c0266798c85d06b56b76d362f7e
elham <elhamrouzban778@yahoo.com>
2022-01-25 01:18:02 +0100

    added context to Person component

--- src/components/person/Persons.js
+++ src/components/person/Persons.js
@@ -1,25 +1,34 @@
 import React from "react";
+import SimpleContext from "../../contex/simpleContex";
+
+
 import Person from "./Person";
 
-const Persons = ({persons , deletPerson, personChange }) => {
-
-    
-    return ( 
-        <div>
-        {persons.map(person => (
-            <Person 
-            key ={person.id} 
-            fullname ={person.fullname} 
-            age ={person.age}
-            deletPerson={() => deletPerson(person.id)}
-            changed = {event =>personChange(event, person.id)}
+const Persons = () => {
 
 
-            />
-            ))}
-            
-            
-            </div>
-            );
-        }
-        export default Persons;
+    return (
+        <SimpleContext.Consumer>
+            {context => (
+                <div>
+                    {context.state.persons.map(person => (
+                        <Person
+                            key={person.id}
+                            fullname={person.fullname}
+                            age={person.age}
+                            deletPerson={() => context.deletPerson(person.id)}
+                            changed={event => context.handelNameChange(event, person.id)}
+                             />
+                    ))}
+
+
+                </div>
+
+
+            )}
+        </SimpleContext.Consumer>
+
+
+    );
+}
+export default Persons;
